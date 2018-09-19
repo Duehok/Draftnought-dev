f25737f75d22b587a2730bb2a99ce8da413bb204
Duehok <36293985+Duehok@users.noreply.github.com>
2018-09-19 20:44:31 +0200

    build work with new folder

--- build.bat
+++ build.bat
@@ -4,8 +4,4 @@ rmdir .\dist /s /q
 rmdir .\build /s /q
 pyinstaller main.py -F --name="Draftnought"
 COPY icon.ico .\dist\icon.ico
-COPY hull_shapes.json .\dist\hull_shapes.json
-COPY lengths.json .\dist\lengths.json
-COPY turrets_outlines.json .\dist\turrets_outlines.json
-COPY turrets_positions.json .\dist\turrets_positions.json
-COPY turrets_scale.json .\dist\turrets_scale.json
+xcopy /s /i ".\data" ".\dist.\data"
