807d186a1147a7e8abeccb76d0a7f14197753823
Duehok <36293985+Duehok@users.noreply.github.com>
2018-09-19 20:49:34 +0200

    add license to dist and run pip correctly

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 pip install pyinstaller
-pip install requirements.txt
+pip install -r requirements.txt
 rmdir .\dist /s /q
 rmdir .\build /s /q
 pyinstaller main.py -F --name="Draftnought"
 COPY icon.ico .\dist\icon.ico
-xcopy /s /i ".\data" ".\dist.\data"
+COPY LICENSE .\dist\LICENSE
+xcopy /s /i ".\data" ".\dist\data"
