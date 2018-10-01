b6154087df3c81bb3cc6e718c0af58c11706cfc5
Duehok <36293985+Duehok@users.noreply.github.com>
2018-10-01 22:00:20 +0200

    small change to build script, more needed

--- build.bat
+++ build.bat
@@ -3,9 +3,9 @@ pip install -r requirements.txt
 rmdir .\dist /s /q
 rmdir .\build /s /q
 pyinstaller main.py -F --name="Draftnought"
-COPY icon.ico .\dist\icon.ico
-COPY "README.md" ".\dist\README.md"
-COPY LICENSE .\dist\LICENSE
+COPY ".\icon.ico" .\dist\icon.ico
+COPY ".\README.md" ".\dist\README.md"
+COPY ".\LICENSE" .\dist\LICENSE
 xcopy /s /i ".\data" ".\dist\data"
 rmdir %localappdata%\Draftnought /s /q
 .\dist\Draftnought.exe
